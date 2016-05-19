5331c26a909615b48b2a65191c81e1fbd35afa7d
Augusto Morais <aflavio@gmail.com>
2016-05-19 17:32:57 -0400

    adding some pattern blocks

--- iptables/iptables-block.sh
+++ iptables/iptables-block.sh
@@ -9,3 +9,19 @@ for i in $IPS
 do
     iptables -A INPUT -p tcp -s $i --dport 80 -j DROP
 done
+
+
+
+# Force SYN packets check
+iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+
+#Force Fragments packets check
+iptables -A INPUT -f -j DROP
+
+#XMAS packets
+iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+
+#DROP all NULL packets
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+
+
