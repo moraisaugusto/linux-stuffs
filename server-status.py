2b37149c4aa56980c050601bd6ebb443b1768514
Augusto Morais <augusto@drunken-angel.drunken-angel.local>
2016-08-24 16:51:12 -0300

    finish first version of server-status

--- server-status.py
+++ server-status.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 
 import subprocess
+import os
+import sys
 
+HEADER = '\033[1;37m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+FAIL = '\033[91m'
+NORMAL = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
 
-def graph(size, title, values):
-    HEADER = '\033[1;37m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    FAIL = '\033[91m'
-    NORMAL = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
+def setTitle(title, size):
     title = " "[:]*5 + title + " "[:]*5 
     print (HEADER + title.center(size+20, "#"))
 
+def graph(size, title, values):
+
+    setTitle(title, size)
     for row in values:
         value = round(float(row[0][:-1]))*size/100
         for i in range(size):
@@ -24,14 +28,15 @@ def graph(size, title, values):
             else:
                 print (YELLOW + "░", end="")
 
-        print (NORMAL + " " + row[0].rjust(4) + " - " + row[1])
+        print (NORMAL + " " + row[0].rjust(6) + " - " + row[1])
 
 def deviceUsage(resource, device=None):
     if resource == "CPU":
         cmd = "cat /proc/stat | awk '/cpu[0-9]/ {value=($2+$4)*100/($2+$4+$5); printf (\" %2.2f%% %s\\n\",value, $1) }'"
-        #cmd = "cat /proc/stat | awk '/cpu[0-9]/ {value=($2+$4)*100/($2+$4+$5); printf (\"%s%s\", value, $1) }'"
     elif resource == "storage":
-        cmd = "df -h | awk '/sda/ {printf (\"%5s %6s\\n\", $5, $6)}'"
+        cmd = "df -h | awk '/"+device+"/ {printf (\"%5s %6s\\n\", $5, $6)}'"
+    elif resource == "memory":
+        cmd = "free -h |tail -n2 | awk  '{value=$3*100/$2; printf (\"%2.2f%% %s \\n\", value, substr($1, 1, length($1) -1))}'"
 
     p = subprocess.Popen(cmd, 
             shell=True, 
@@ -48,9 +53,82 @@ def deviceUsage(resource, device=None):
     return process
 
 
-cpu = deviceUsage("CPU")
-storage=deviceUsage("storage")
+def lastLogins(size, title, n = 3):
 
-graph(40, "Storage", storage)
-print()
-graph(40, "CPU", cpu)
+    setTitle(title, size)
+    cmd = "last | head -n"+str(n)+" | awk '{name=substr($0,0,7); data=substr($0,40,16);status=substr($0,59,5); printf (\"%s|%s|%s\\n\", name, data, status )}' "
+    p = subprocess.Popen(cmd, 
+            shell=True, 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.STDOUT,
+            universal_newlines = True)
+
+    for line in p.stdout.readlines():
+        detail = line.strip()
+        detail = detail.split("|")
+        formatedDetail = UNDERLINE+detail[0] + "\t\t" + NORMAL+detail[1] + "\t" + BOLD+detail[2]
+        print (formatedDetail)
+    retval = p.wait()
+
+
+def distroDetail(size, title):
+
+    setTitle(title, size)
+    getDistro = "uname -a | awk '{ printf (\"%s|%s|%s\", $6, $7, $9)}'"
+    getUptime = "uptime -p | awk '{data=substr($0,4,40);printf (\"%s\", data)}'"
+    getLogged = "w | awk 'FNR > 2 {print $0}' | wc -l"
+    cmds = [getDistro, getUptime, getLogged]
+
+    result = []
+    for i in cmds:
+        p = subprocess.Popen(i, 
+                shell=True, 
+                stdout=subprocess.PIPE, 
+                stderr=subprocess.STDOUT,
+                universal_newlines = True)
+
+        resultRaw = p.stdout.readlines();
+        resultRaw= ' '.join(resultRaw).rstrip().split("|")
+        result += resultRaw
+
+    labels = [BOLD + "Distro: ",
+              BOLD + "\t\tKernel: ",
+              BOLD + "\t\tARCH: ",
+              BOLD + "\nUptime: ",
+              BOLD + "\tLogged Users: "]
+
+    formatedDetail = ''
+    for i in range(len(labels)):
+        formatedDetail +=  labels[i] + BLUE + result[i] + NORMAL
+
+    print (formatedDetail)
+
+
+def main(argv):
+    os.system("clear")
+    size = 40
+
+    for arg in argv:
+        if arg == "--cpu":
+            cpu = deviceUsage("CPU")
+            graph(size, "CPU", cpu)
+            print()
+        elif arg == "--memory":
+            memory=deviceUsage("memory")
+            graph(size, "Memory", memory)
+            print()
+        elif "--storage" in arg:
+            device = arg.split("=")
+            storage=deviceUsage("storage", device[1])
+            graph(size, "Storage", storage)
+            print()
+        elif arg == "--lastlogins":
+            lastLogins(size, "Last Logins", 3)
+            print()
+        elif arg == "--distroDetail":
+            distroDetail(size, "Distro Details")
+            print()
+
+if __name__ == "__main__":
+    main(sys.argv)
+
